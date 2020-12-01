310b4aa5e41b4e7eb6978cec407928c942b0993b
aendrevv <andrew.tolmachov@gmail.com>
2020-12-01 18:55:00 +0200

    total folders restructuring

--- homework-03/src/app.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const http = require('http');
-const requestsHandler = require('./requestHandler');
-
-const port = +process.env.PORT || 5000;
-
-const server = http.createServer(requestsHandler);
-server.listen(port, () => console.log(`🤖 Server is listening on port ${port}!`));
