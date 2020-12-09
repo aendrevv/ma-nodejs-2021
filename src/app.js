cca0e6e4ebc486be07d13d21954750a51889b645
aendrevv <andrew.tolmachov@gmail.com>
2020-12-09 18:17:57 +0200

    Current progres

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-const { server } = require('./config');
+const { server, db: dbConfig } = require('./config');
 const app = require('./server');
 
-app.listen(server.PORT, () => {
-  console.log(`👾 App is listening at http://${server.HOST}:${server.PORT}`);
-});
+const db = require('./db')(dbConfig);
+
+const boot = async () => {
+  try {
+    await db.testConnection();
+
+    // const p = await db.createProduct({ type: 'socks', color: 'red', price: 14.88 });
+    // console.log(`p: ${JSON.stringify(p)}`);
+
+    app.listen(server.PORT, () => {
+      console.log(`👾 App is listening at http://${server.HOST}:${server.PORT}`);
+    });
+  } catch (error) {
+    console.error(error.message || error);
+    throw error;
+  }
+};
+
+boot();
--- src/config/index.js
+++ src/config/index.js
@@ -13,6 +13,14 @@ const config = {
     PASSWORD: process.env.USER_PASSWORD || fatal('No user password'),
   },
 
+  db: {
+    user: process.env.DB_USER || fatal('FATAL: DB_USER is not defined'),
+    host: process.env.DB_HOST || fatal('FATAL: DB_HOST is not defined'),
+    port: process.env.DB_PORT || fatal('FATAL: DB_PORT is not defined'),
+    database: process.env.DB_NAME || fatal('FATAL: DB_NAME is not defined'),
+    password: process.env.DB_PASS || fatal('FATAL: DB_PASS is not defined'),
+  },
+
   folders: {
     UPLOAD: process.env.UPLOAD || `${process.cwd()}/uploads`,
   },
